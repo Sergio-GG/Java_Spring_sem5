6c017d8f5e0aba1b73a9d88af253c29a25bf2cdb
Sergio <sergio-guitar@yandex.ru>
2024-05-28 19:44:26 +0300

    seminar_10 (Mocking)

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example;
 
+import com.example.model.Task;
+import com.example.repository.TaskRepository;
+import com.example.service.TaskService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@InjectMocks
+	TaskService taskService;
+
+	@Mock
+	TaskRepository taskRepository;
+
 	@Test
 	void contextLoads() {
+		// given
+		Task task = new Task();
+		task.setDescription("New task");
+
+		// when
+		when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+
+		// then
+		Assertions.assertEquals("New task", taskService.getTaskById(1L).getDescription());
+		verify(taskRepository,times(1)).findById(1L);
 	}
 
 }
